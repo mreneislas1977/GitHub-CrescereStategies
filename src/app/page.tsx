dbd212b59e60de680617a558b4be705a399bcda6
M Rene Islas <mreneislas@crescere-strat.com>
2026-02-05 00:58:54 +0000

    Build 28: Fixed directive order for Turbopack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 export const dynamic = "force-dynamic";
+
 import { db } from '../firebaseConfig';
 import { collection, getDocs, limit, query } from 'firebase/firestore';
 import { useState, useEffect } from 'react';
