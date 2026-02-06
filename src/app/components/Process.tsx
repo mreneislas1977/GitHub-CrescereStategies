adc4b2e951b76919853e986f374f7ae98ef21461
M Rene Islas <mreneislas@crescere-strat.com>
2026-02-06 02:14:08 +0000

    Fix file corruption in Process component

--- src/app/components/Process.tsx
+++ src/app/components/Process.tsx
@@ -62,20 +62,3 @@ const Process: React.FC = () => {
 };
 
 export default Process;
-EOFh
-cat <<'EOF' > src/app/page.tsx
-import Header from './components/Header';
-import Hero from './components/Hero';
-import CollaborativePower from './components/CollaborativePower';
-import Process from './components/Process';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <Header />
-      <Hero />
-      <CollaborativePower />
-      <Process />
-    </main>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
 import CollaborativePower from './components/CollaborativePower';
+import Process from './components/Process';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <CollaborativePower />
+      <Process />
     </main>
   );
 }
