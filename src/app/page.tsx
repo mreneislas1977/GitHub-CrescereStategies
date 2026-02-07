0c6f11bdf8239319c358c544fc6318324ad4f5c5
M Rene Islas <mreneislas@crescere-strat.com>
2026-02-07 02:39:12 +0000

    fix: standardized app directory structure and relative imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Header from '@/components/Header';
-import Hero from '@/components/Hero';
-import CollaborativePower from '@/components/CollaborativePower';
-import Process from '@/components/Process';
-import Stakeholders from '@/components/Stakeholders';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import CollaborativePower from './components/CollaborativePower';
+import Process from './components/Process';
+import Stakeholders from './components/Stakeholders';
 
 export default function Home() {
   return (
