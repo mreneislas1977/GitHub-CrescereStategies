0ac57fb04ea049e62c2a8845c0c41934e72dc241
M Rene Islas <mreneislas@crescere-strat.com>
2026-02-07 02:16:58 +0000

    feat: initialize firebase directory and connection

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMoSO2-v0cLMQJddMfuYL-L7w17u0FFmo",
+  authDomain: "gen-lang-client-0572328590.firebaseapp.com",
+  projectId: "gen-lang-client-0572328590",
+  storageBucket: "gen-lang-client-0572328590.firebasestorage.app",
+  messagingSenderId: "782480574555",
+  appId: "1:782480574555:web:ddbec0e2881ab9dca9977e",
+  measurementId: "G-H0Q4W3QH4S"
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { db };
