34abcb445d7907663c07c77f770afa9a8dfc54d6
mreneislas1977 <mreneislas@crescere-strat.com>
2026-02-14 10:35:45 -0500

    Fix PainPoints import and update main background color
    Fixed the import statement for PainPoints and updated the main background color.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
 import Header from './Header';
 import Hero from './Hero';
-import PainPoints from './'PainPoints':
+import PainPoints from './PainPoints'; // Fixed Import
 import CollaborativePower from './CollaborativePower';
 import Process from './Process';
 import Stakeholders from './Stakeholders';
-import LeadershipBio from './LeadershipBio';
 import Footer from './Footer';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-crescere-cream">
+    <main className="min-h-screen bg-[#fdfbf5]">
       <Header />
+      
+      {/* 1. Hero Section */}
       <Hero />
+      
+      {/* 2. Pain Points (Agitation) */}
       <PainPoints />
+      
+      {/* 3. Solutions & Process */}
       <CollaborativePower />
       <Process />
-      <LeadershipBio />
       <Stakeholders />
+      
+      {/* 4. Footer */}
       <Footer />
     </main>
   );
