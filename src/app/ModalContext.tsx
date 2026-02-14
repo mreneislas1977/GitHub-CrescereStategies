e787e379e841df54d80c505e29d3b0662a6826fe
mreneislas1977 <mreneislas@crescere-strat.com>
2026-02-14 09:42:21 -0500

    Refactor ModalContext to use ModalProvider

--- src/app/ModalContext.tsx
+++ src/app/ModalContext.tsx
@@ -1,15 +1,9 @@
-'use client';
-import React, { createContext, useContext, useState } from 'react';
-const ModalContext = createContext({ isModalOpen: false, openModal: () => {}, closeModal: () => {} });
-export const ModalProvider = ({ children }: { children: React.ReactNode }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+import { ModalProvider } from './ModalContext'; 
+
+export default function Layout({ children }) {
   return (
-    <ModalContext.Provider value={{ isModalOpen, openModal, closeModal }}>
+    <ModalProvider>
       {children}
-    </ModalContext.Provider>
+    </ModalProvider>
   );
-};
-export const useModal = () => useContext(ModalContext);
-// Force Build Sat Feb  7 03:01:50 AM UTC 2026
+}
